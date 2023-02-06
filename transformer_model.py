7a977a71031b64c74522cf84cd34895883c054f5
rmjrmj <49266372+rmjrmj@users.noreply.github.com>
2023-02-06 12:30:45 +0800

    Add files via upload

---
+++ transformer_model.py
@@ -0,0 +1,368 @@
+from __future__ import absolute_import, division, print_function
+
+import argparse
+import csv
+import logging
+import os
+import random
+import pickle
+import sys
+# from global_config import *
+import math
+from torch.nn.utils.rnn import pad_sequence
+import copy
+
+import numpy as np 
+from sklearn.metrics.pairwise import cosine_distances
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, TensorDataset
+from torch.utils.data.distributed import DistributedSampler
+from transformers import (
+    AlbertModel,
+    AlbertPreTrainedModel,
+    AlbertConfig,
+    load_tf_weights_in_albert,
+)
+from einops import rearrange, reduce, repeat
+# from transformers.modeling_albert import AlbertEmbeddings, AlbertLayerGroup
+# from transformers import AlbertEmbeddings, AlbertLayerGroup
+#from transformers.models.modeling_albert import AlbertEmbeddings, AlbertLayerGroup
+
+
+
+"""
+Implementation taken from: 
+https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+"""
+class MatchingAttention(nn.Module):
+    
+    def __init__(self, mem_dim, cand_dim, alpha_dim=None, att_type='general'):
+        super(MatchingAttention, self).__init__()
+        assert att_type!='concat' or alpha_dim!=None
+        assert att_type!='dot' or mem_dim==cand_dim
+        self.mem_dim = mem_dim
+        self.cand_dim = cand_dim
+        self.att_type = att_type
+        if att_type=='general':
+            self.transform = nn.Linear(cand_dim, mem_dim, bias=False)
+        if att_type=='general2':
+            self.transform = nn.Linear(cand_dim, mem_dim, bias=True)
+            #torch.nn.init.normal_(self.transform.weight,std=0.01)
+        elif att_type=='concat':
+            self.transform = nn.Linear(cand_dim+mem_dim, alpha_dim, bias=False)
+            self.vector_prod = nn.Linear(alpha_dim, 1, bias=False)
+
+    def forward(self, M, x, mask=None):
+        """
+        M -> (seq_len, batch, mem_dim)
+        x -> (batch, cand_dim)
+        mask -> (batch, seq_len)
+        """
+        if type(mask)==type(None):
+            mask = torch.ones(M.size(1), M.size(0)).type(M.type())
+
+        if self.att_type=='dot':
+            # vector = cand_dim = mem_dim
+            M_ = M.permute(1,2,0) # batch, vector, seqlen
+            x_ = x.unsqueeze(1) # batch, 1, vector
+            alpha = F.softmax(torch.bmm(x_, M_), dim=2) # batch, 1, seqlen
+        elif self.att_type=='general':
+            M_ = M.permute(1,2,0) # batch, mem_dim, seqlen
+            x_ = self.transform(x).unsqueeze(1) # batch, 1, mem_dim
+            alpha = F.softmax(torch.bmm(x_, M_), dim=2) # batch, 1, seqlen
+        elif self.att_type=='general2':
+            M_ = M.permute(1,2,0) # batch, mem_dim, seqlen
+            x_ = self.transform(x).unsqueeze(1) # batch, 1, mem_dim
+            mask_ = mask.unsqueeze(2).repeat(1, 1, self.mem_dim).transpose(1, 2) # batch, seq_len, mem_dim
+            M_ = M_ * mask_
+            alpha_ = torch.bmm(x_, M_)*mask.unsqueeze(1)
+            alpha_ = torch.tanh(alpha_)
+            alpha_ = F.softmax(alpha_, dim=2)
+            # alpha_ = F.softmax((torch.bmm(x_, M_))*mask.unsqueeze(1), dim=2) # batch, 1, seqlen
+            alpha_masked = alpha_*mask.unsqueeze(1) # batch, 1, seqlen
+            alpha_sum = torch.sum(alpha_masked, dim=2, keepdim=True) # batch, 1, 1
+            alpha = alpha_masked/alpha_sum # batch, 1, 1 ; normalized
+            #import ipdb;ipdb.set_trace()
+        else:
+            M_ = M.transpose(0,1) # batch, seqlen, mem_dim
+            x_ = x.unsqueeze(1).expand(-1,M.size()[0],-1) # batch, seqlen, cand_dim
+            M_x_ = torch.cat([M_,x_],2) # batch, seqlen, mem_dim+cand_dim
+            mx_a = F.tanh(self.transform(M_x_)) # batch, seqlen, alpha_dim
+            alpha = F.softmax(self.vector_prod(mx_a),1).transpose(1,2) # batch, 1, seqlen
+
+        attn_pool = torch.bmm(alpha, M.transpose(0,1))[:,0,:] # batch, mem_dim
+        return attn_pool, alpha
+
+class GRUModel(nn.Module):
+    
+    def __init__(self, D_m, D_e, D_h, n_classes=7, dropout=0.5):
+        
+        super(GRUModel, self).__init__()
+        
+        self.n_classes = n_classes
+        self.dropout   = nn.Dropout(dropout)
+        self.gru = nn.GRU(input_size=D_m, hidden_size=D_e, num_layers=2, bidirectional=True, dropout=dropout)
+        self.matchatt = MatchingAttention(2*D_e, 2*D_e, att_type='general2')
+        self.linear = nn.Linear(2*D_e, D_h)
+        self.smax_fc = nn.Linear(D_h, n_classes)
+        
+    def forward(self, U, qmask, umask, att2=False):
+        """
+        U -> seq_len, batch, D_m
+        qmask -> seq_len, batch, party
+        """
+        emotions, hidden = self.gru(U)
+        alpha, alpha_f, alpha_b = [], [], []
+        
+        if att2:
+            att_emotions = []
+            alpha = []
+            for t in emotions:
+                att_em, alpha_ = self.matchatt(emotions,t,mask=umask)
+                att_emotions.append(att_em.unsqueeze(0))
+                alpha.append(alpha_[:,0,:])
+            att_emotions = torch.cat(att_emotions,dim=0)
+            hidden = F.relu(self.linear(att_emotions))
+        else:
+            hidden = F.relu(self.linear(emotions))
+        
+        # hidden = F.relu(self.linear(emotions))
+        hidden = self.dropout(hidden)
+        # log_prob = F.log_softmax(self.smax_fc(hidden), 2)
+        # return log_prob, alpha, alpha_f, alpha_b, emotions
+        return hidden
+
+
+class LSTMModel(nn.Module):
+    
+    def __init__(self, D_m, D_e, D_h, n_classes=7, dropout=0.5):
+        
+        super(LSTMModel, self).__init__()
+        
+        self.n_classes = n_classes
+        self.dropout = nn.Dropout(dropout)
+        self.lstm = nn.LSTM(input_size=D_m, hidden_size=D_e, num_layers=2, bidirectional=True, dropout=dropout)
+        self.matchatt = MatchingAttention(2*D_e, 2*D_e, att_type='general2')
+        self.linear = nn.Linear(2*D_e, D_h)
+        self.smax_fc = nn.Linear(D_h, n_classes)
+
+    def forward(self, U, qmask, umask, att2=False):
+        """
+        U -> seq_len, batch, D_m
+        qmask -> seq_len, batch, party
+        """
+        emotions, hidden = self.lstm(U)
+        alpha, alpha_f, alpha_b = [], [], []
+        
+        if att2:
+            att_emotions = []
+            alpha = []
+            for t in emotions:
+                att_em, alpha_ = self.matchatt(emotions, t, mask=umask)
+                att_emotions.append(att_em.unsqueeze(0))
+                alpha.append(alpha_[:,0,:])
+            att_emotions = torch.cat(att_emotions,dim=0)
+            hidden = F.relu(self.linear(att_emotions))
+        else:
+            hidden = F.relu(self.linear(emotions))
+        
+        # hidden = F.relu(self.linear(emotions))
+        hidden = self.dropout(hidden)
+        #log_prob = F.log_softmax(self.smax_fc(hidden), 2)
+        return hidden
+
+
+class MaskedNLLLoss(nn.Module):
+    
+    def __init__(self, weight=None):
+        super(MaskedNLLLoss, self).__init__()
+        self.weight = weight
+        self.loss = nn.NLLLoss(weight=weight,
+                               reduction='sum')
+
+    def forward(self, pred, target, mask):
+        """
+        pred -> batch*seq_len, n_classes
+        target -> batch*seq_len
+        mask -> batch, seq_len
+        """
+        mask_ = mask.view(-1,1) # batch*seq_len, 1
+        if type(self.weight)==type(None):
+            loss = self.loss(pred*mask_, target)/torch.sum(mask)
+        else:
+            loss = self.loss(pred*mask_, target)\
+                            /torch.sum(self.weight[target]*mask_.squeeze())
+        return loss
+
+
+class Transformer(nn.Module):
+    def __init__(self, d_model, num_layers=1, nhead=1, dropout=0.1, dim_feedforward=128, max_seq_length=5000):
+        super(Transformer, self).__init__()
+        self.d_model = d_model
+        self.pos_encoder = nn.Embedding(max_seq_length, d_model)
+        self.encoder = TransformerEncoder(TransformerLayer(d_model, nhead=nhead, dim_feedforward=dim_feedforward, dropout=dropout), num_layers=num_layers)
+        self.decoder = nn.Linear(d_model, 1)
+        self.norm = nn.LayerNorm(d_model)
+        self.cls_token = nn.Parameter(torch.randn(1,1, 1024+768))
+
+    def forward(self, input, attention_mask=None):
+        cls_tokens = repeat(self.cls_token, '() n e -> b n e', b=input.size()[0])
+        seq_length = input.size()[1]
+        position_ids = torch.arange(seq_length+1, dtype=torch.long, device=input.device)
+        # positions_embedding = self.pos_encoder(position_ids).unsqueeze(0).expand(input.size()) # (seq_length, d_model) => (batch_size, seq_length, d_model)
+        positions_embedding = self.pos_encoder(position_ids).unsqueeze(0).expand([input.size()[0], int(seq_length)+1, 1024+768]) # (seq_length, d_model) => (batch_size, seq_length, d_model)
+        input = torch.cat([cls_tokens, input], dim=1)
+        input = input + positions_embedding
+        input = self.norm(input)
+        hidden = self.encoder(input, attention_mask=attention_mask)
+        # out = self.decoder(hidden) # (batch_size, seq_len, hidden_dim)
+        # out = (out[:,0,:], out, hidden) # ([CLS] token embedding, full output, last hidden layer)
+        out = (hidden[:,0,:], hidden[:,1:,:])
+        return out
+
+
+class TransformerLayer(nn.Module):
+    def __init__(self, hidden_size, nhead=1, dim_feedforward=128, dropout=0.1):
+        super(TransformerLayer, self).__init__()
+        self.self_attention = Attention(hidden_size, nhead, dropout)
+        self.fc = nn.Sequential(nn.Linear(hidden_size, dim_feedforward), nn.ReLU(), nn.Linear(dim_feedforward, hidden_size))
+        self.norm1 = nn.LayerNorm(hidden_size)
+        self.norm2 = nn.LayerNorm(hidden_size)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+    def forward(self, src, attention_mask=None):
+        src_1 = self.self_attention(src, src, attention_mask=attention_mask)
+        src = src + self.dropout1(src_1)
+        src = self.norm1(src)
+        src_2 = self.fc(src)
+        src = src + self.dropout2(src_2)
+        src = self.norm2(src)
+
+        return src
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, layer, num_layers):
+        super(TransformerEncoder, self).__init__()
+        self.layers = _get_clones(layer, num_layers)
+    def forward(self, src, attention_mask=None):
+        for layer in self.layers:
+            new_src = layer(src, attention_mask=attention_mask)
+            src = src + new_src
+        return src
+
+
+class Attention(nn.Module):
+    def __init__(self, hidden_size, num_attention_heads, attention_probs_dropout_prob, ctx_dim=None):
+        super().__init__()
+        if hidden_size % num_attention_heads != 0:
+            raise ValueError(
+                "The hidden size (%d) is not a multiple of the number of attention "
+                "heads (%d)" % (hidden_size, num_attention_heads))
+        self.num_attention_heads = num_attention_heads
+        self.attention_head_size = int(hidden_size / num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        # visual_dim = 2048
+        if ctx_dim is None:
+            ctx_dim = hidden_size
+        self.query = nn.Linear(hidden_size, self.all_head_size)
+        self.key = nn.Linear(ctx_dim, self.all_head_size)
+        self.value = nn.Linear(ctx_dim, self.all_head_size)
+
+        self.dropout = nn.Dropout(attention_probs_dropout_prob)
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(self, hidden_states, context, attention_mask=None):
+        mixed_query_layer = self.query(hidden_states)
+        mixed_key_layer = self.key(context)
+        mixed_value_layer = self.value(context)
+
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+        key_layer = self.transpose_for_scores(mixed_key_layer)
+        value_layer = self.transpose_for_scores(mixed_value_layer)
+
+        # Take the dot product between "query" and "key" to get the raw attention scores.
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+
+        # Apply the attention mask is 
+        if attention_mask is not None:
+            attention_scores = attention_scores + attention_mask
+
+        # Normalize the attention scores to probabilities.
+        attention_probs = nn.Softmax(dim=-1)(attention_scores)
+
+        # This is actually dropping out entire tokens to attend to, which might
+        # seem a bit unusual, but is taken from the original Transformer paper.
+        attention_probs = self.dropout(attention_probs)
+
+        context_layer = torch.matmul(attention_probs, value_layer)
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+        return context_layer
+
+
+class CrossAttentionLayer(nn.Module):
+    def __init__(self, hidden_size, context_size, nhead=1, dropout=0.1):
+        super(CrossAttentionLayer, self).__init__()
+        self.src_cross_attention = Attention(hidden_size, nhead, dropout, ctx_dim=context_size)
+        self.context_cross_attention = Attention(context_size, nhead, dropout, ctx_dim=hidden_size)
+        self.self_attention = Attention(hidden_size + context_size, nhead, dropout)
+        self.fc = nn.Sequential(nn.Linear(hidden_size + context_size, hidden_size + context_size), nn.ReLU())
+        self.norm1 = nn.LayerNorm(hidden_size + context_size)
+        self.norm2 = nn.LayerNorm(hidden_size + context_size)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+    def forward(self, src, context, attention_mask=None):
+        new_src = self.src_cross_attention(src, context, attention_mask=attention_mask)
+        new_context = self.context_cross_attention(context, src, attention_mask=attention_mask)
+        
+        cross_src = torch.cat((new_src, new_context), dim=2)
+        
+        cross_src_1 = self.self_attention(cross_src, cross_src, attention_mask)
+        cross_src = cross_src + self.dropout1(cross_src_1)
+        cross_src = self.norm1(cross_src)
+
+        cross_src_2 = self.fc(cross_src)
+        cross_src = cross_src + self.dropout2(cross_src_2)
+        cross_src = self.norm2(cross_src)
+
+        return cross_src
+
+class CrossAttentionEncoder(nn.Module):
+    def __init__(self, layer, num_layers):
+        super(CrossAttentionEncoder, self).__init__()
+        self.layers = _get_clones(layer, num_layers)
+
+    def forward(self, src, context, attention_mask=None):
+        src_dim = src.size()[2]
+        context_dim = context.size()[2]
+
+        for layer in self.layers:
+            output = layer(src, context, attention_mask=attention_mask)
+            new_src = output[:,:,0:src_dim]
+            new_context = output[:,:,src_dim:src_dim+context_dim]
+
+            src = src + new_src
+            context = context + new_context
+
+        return output
+
+
+
+def _get_clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
+
+
+
+
